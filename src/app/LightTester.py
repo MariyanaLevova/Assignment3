c0b3a541b00a596b490bc71a2c75ee6cc2721d3b
MariyanaLevova <35726481+MariyanaLevova@users.noreply.github.com>
2018-02-28 18:14:47 +0000

    fixing class

--- src/app/LightTester.py
+++ src/app/LightTester.py
@@ -1,21 +1,28 @@
+import re #regex module
+import urllib.request
+import requests
+import urllib # to open and read from a URL
+
 class LightTester(object):
      
-    lights = None
     def __init__(self,N):
         self.size = N
         self.lights = [[False]*N for _ in range(N)]
               
-    def apply(self,cmd):
-        if cmd is 'switch on':
-            self.lights[0][1] = True
-        elif cmd is 'switch off':
-            self.lights[0][1] = False
-        elif cmd is 'toggle':
-            if self.lights[0][1] == True:
-                self.lights[0][1] = False
-            else:
-                self.lights[0][1] = True
-    
+    def apply(self,cmd,x,y,x1,y1):
+        
+        for i in range(y,y1+1): # arrays
+            for j in range(x,x1+1): 
+                if cmd == 'turn on':# elements
+                    self.lights[i][j] = True
+                elif cmd == 'turn off':
+                    self.lights[i][j] = True
+                elif cmd == 'switch':
+                    if self.lights[i][j] == True:
+                        self.lights[i][j] = False
+                    else:
+                        self.lights[i][j] = True
+        
     def count(self):
         count = 0
         for i in self.lights:
@@ -24,14 +31,21 @@ class LightTester(object):
                     count+=1
         return count
 
-    def coordinates(self,x,y,x1,y1):
-        for i in range(y,y1+1):
-            for j in range(x,x1+1):
-                self.lights[i][j] = True
-        for i in self.lights:
-            print(i)
-        return(self.count())
+    def parse(self,filename):
+        pat = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+        if filename == 'data':
+            fh= requests.get("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
+        elif filename == 'dataA':
+            fh= requests.get("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_a.txt")
+                        
+        file1 = fh.text.split('\n')
+        for line in file1[ :1]:
+            N = line
 
+        for line in file1[1:-1]:
+            sort = pat.match(line) #matches the regex
+            arr = [sort.group(1),sort.group(2),sort.group(3),sort.group(4),sort.group(5)]
+            yield arr
 
 if __name__ == "__main__":
     LightTester().__init__
