b16241c4ec3382b4041907e134dc7c7980e2b398
MariyanaLevova <35726481+MariyanaLevova@users.noreply.github.com>
2018-02-28 21:53:13 +0000

    Update command_line.py

--- src/app/command_line.py
+++ src/app/command_line.py
@@ -3,7 +3,14 @@ from main import *
 
 def run():
     x = input("prompt: ")
-    if x == 'data/input_assign3.txt':
-        
+    if x == 'data/input_assign3.txt' or x == 'http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt':
         m.first()
+    elif x == 'data/input_assign3_a.txt' or x == 'http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_a.txt':
+        m.A()
+    elif x == 'data/input_assign3_b.txt' or x == 'http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_b.txt':
+        m.B()
+    elif x == 'data/input_assign3_c.txt' or x == 'http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_c.txt':
+        m.C()
+    elif x == 'data/input_assign3_d.txt' or x == 'http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_d.txt':
+        m.D()
 run()
