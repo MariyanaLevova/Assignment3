7372c1f806141d733386f73cc31f4375ff7f12dc
MariyanaLevova <35726481+MariyanaLevova@users.noreply.github.com>
2018-02-28 19:52:52 +0000

    Update LightTester.py
    debugging

--- src/app/LightTester.py
+++ src/app/LightTester.py
@@ -11,17 +11,36 @@ class LightTester(object):
               
     def apply(self,cmd,x,y,x1,y1):
         
+        if x < 0:
+            x = 0
+        if x > self.size-1:
+            x = self.size-1
+        if x1 < 0:
+            x1 = 0
+        if x1 > self.size-1:
+            x1 = self.size-1
+        if y < 0:
+            y = 0
+        if y > self.size-1:
+            y = self.size-1
+        if y1 < 0:
+            y1 = 0
+        if y1 > self.size-1:
+            y1 = self.size-1
+        
         for i in range(y,y1+1): # arrays
             for j in range(x,x1+1): 
                 if cmd == 'turn on':# elements
                     self.lights[i][j] = True
                 elif cmd == 'turn off':
-                    self.lights[i][j] = True
+                    self.lights[i][j] = False
                 elif cmd == 'switch':
                     if self.lights[i][j] == True:
                         self.lights[i][j] = False
                     else:
                         self.lights[i][j] = True
+                else:
+                    continue
         
     def count(self):
         count = 0
@@ -37,15 +56,23 @@ class LightTester(object):
             fh= requests.get("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
         elif filename == 'dataA':
             fh= requests.get("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_a.txt")
+        elif filename == 'dataB':
+            fh= requests.get("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_b.txt")
+        elif filename == 'dataC':
+            fh= requests.get("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_c.txt")
+        elif filename == 'dataD':
+            fh= requests.get("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_d.txt")
                         
         file1 = fh.text.split('\n')
-        for line in file1[ :1]:
-            N = line
 
         for line in file1[1:-1]:
-            sort = pat.match(line) #matches the regex
-            arr = [sort.group(1),sort.group(2),sort.group(3),sort.group(4),sort.group(5)]
+            sort = pat.match(line)
+            if sort: #matches the regex
+                arr = [sort.group(1),sort.group(2),sort.group(3),sort.group(4),sort.group(5)]
+            else:
+                continue 
             yield arr
+        fh.close()
 
 if __name__ == "__main__":
     LightTester().__init__
