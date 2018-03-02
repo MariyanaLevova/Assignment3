fe3b2d996bdb64b10f09e4332fb6a5c597942455
Mariyana Levova <mariyana.levova@ucdconnect.ie>
2018-03-02 14:22:13 +0000

    applying more test

--- src/app/test.py
+++ src/app/test.py
@@ -7,22 +7,24 @@ class Test(unittest.TestCase):
     
     def test_apply(self):
         a = LightTester(3)
-        a.apply('toggle')
+        a.apply('switch', 0,0,1,1)
         self.assertTrue(a.lights[0][1] == True)
+        b = LightTester(5)
+        b.apply('turn on',1,1,3,3)
+        self.assertTrue(b.lights[3][4] == False)
+        self.assertTrue(b.lights[2][3] == True)
+        b.apply('turn off',3,0,3,3)
+        self.assertTrue(b.lights[3][3] == False)
 
     def test_count(self):
         count = 0
         a = LightTester(3)
-        cmd = "toggle"
-        a.apply(cmd)
-        self.assertTrue(a.count() == 1)
+        x,y,x1,y1 = 1,1,2,2
+        cmd = "switch"
+        a.apply(cmd,x,y,x1,y1)
+        self.assertTrue(a.count() == 4)
 
-    def test_coordinates(self):
-        a=LightTester(5)
-        x,y,x1,y1 = 1,1,2,4
-        a.coordinates(x,y,x1,y1)
-        print(a.count())
-        self.assertTrue(a.count() == 8)
+    
 
 if __name__ == "__main__":
     suite = unittest.defaultTestLoader.loadTestsFromTestCase(Test)
